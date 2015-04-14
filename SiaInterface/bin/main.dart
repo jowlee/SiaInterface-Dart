b63e557daba5b9b029e2ee89ebc398f4036d87da
AndrewKaiser <kaisea@rpi.edu>
2015-04-14 16:22:28 -0400

    follow my format guys

--- SiaInterface/bin/main.dart
+++ SiaInterface/bin/main.dart
@@ -3,6 +3,24 @@
 
 import 'package:SiaInterface/SiaInterface.dart' as SiaInterface;
 
-main() {
-  print('Hello world: ${SiaInterface.calculate()}!');
+void parseHost(var request) {
+  if (request.substring(4,12) == "announce") {
+    // do announce stuff
+  }
+  else if (request.substring(4,12) == "config") {
+    // do config stuff
+  }
+}
+
+main(List<String> arguments) {
+  if (arguments.length == 1) {
+    return 0;
+  }
+  // one arguement allowed
+  // $ dart main.dart host/config
+  var request = arguments[0];
+  if (arguments[0].toLowerCase().substring(0, 4) == "host"){
+    parseHost(arguments[0]);
+  }
+  
 }
