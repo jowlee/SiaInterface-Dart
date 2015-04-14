c7e04ebdbc1fce0de0c8af69c4099a74c37ed427
AndrewKaiser <kaisea@rpi.edu>
2015-04-14 17:57:40 -0400

    ccchanges

--- SiaInterface/bin/main.dart
+++ SiaInterface/bin/main.dart
@@ -2,12 +2,19 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:SiaInterface/SiaInterface.dart' as SiaInterface;
+//import 'dart:html';
+//import 'dart:io';
+
+void outputInfo(var jsonString) {
+  print(jsonString);
+}
 
 void parseHost(var requestList) {
   var request = requestList[0].substring(4,requestList[0].length);
   switch (request) {
     case 'announce':
-      
+      var url = "http://localhost:9980/host/announce";
+        var jsonString = HttpRequest.getString(url).then(outputInfo);
       break;
     case 'config':
       
@@ -28,7 +35,8 @@ main(List<String> arguments) {
   // $ dart main.dart host/config parameters
   var request = arguments[0];
   switch (request) {
-    case request.substring(0,6) == 'daemon':
+    case 'daemon':
+      print("test");
 //      parseDaemon(arguments);
       break;
     case 'consensus':
