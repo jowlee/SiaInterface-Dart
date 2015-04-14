1a15c208d3587a9a70d55d9a6fe7b5c96699ee13
Joseph Lee <leej42@rpi.edu>
2015-04-14 17:42:44 -0400

    framework of implementation

--- SiaInterface/bin/main.dart
+++ SiaInterface/bin/main.dart
@@ -2,6 +2,7 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:SiaInterface/SiaInterface.dart' as SiaInterface;
+import 'dart:html';
 
 void parseHost(var requestList) {
   var request = requestList[0].substring(4,requestList[0].length);
@@ -10,6 +11,23 @@ void parseHost(var requestList) {
   }
   else if (request == "config") {
     // do config stuff
+  }
+}
+
+void renter(var param) {
+  
+  if (param.substring(7, 15) == "downloadqueue") {
+    // do downloadqueue
+  }
+  else if (param.substring(7, 15) == "download") {
+    // do download stuff
+  }
+  else if (param.substring(7, 12) == "files") {
+    // do files stuff
+  }
+  else if (param.substring(7, 13) == "upload") {
+    HttpRequest.getString('http://localhost:9980').then(
+        (result) => print('Request complete: $result'));
   }
 }
 
@@ -22,28 +40,28 @@ main(List<String> arguments) {
   var request = arguments[0];
   switch (request) {
     case '/daemon':
-      executeClosed();
+//      executeClosed();
       break;
     case '/consensus':
-      executeOpen();
+//      executeOpen();
       break;
     case '/gateway':
-      executeApproved();
+//      executeApproved();
       break;
     case '/host':
-      executeApproved();
+//      executeApproved();
       break;
     case '/miner':
-      executeApproved();
+//      executeApproved();
       break;
-    case '/renter':
-      executeApproved();
+    case request.substring(0,5) == 'renter':
+      renter(request);
       break;
-    case '/transactionpool':
-      executeApproved();
+    case request.substring(0,15) == 'transactionpool':
+//      executeApproved();
       break;
-    case '/wallet':
-      executeApproved();
+    case request.substring(0,6) == 'wallet':
+//      executeApproved();
       break;      
     default:
       print('nothing works');
